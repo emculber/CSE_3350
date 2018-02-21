33ced319c7e68e16c7008f27428b1fd7e382f9e5
Erik <eculberson12@gmail.com>
2018-02-21 17:35:47 -0500

    Homework

---
+++ homework_3/IMP1/imp.cpp
@@ -0,0 +1,201 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <map>
+#include <list>
+#include "imp.h"
+
+using namespace std;
+
+  // the constructor for node links the node to its children,
+  // and stores the character representation of the operator.
+  binaryOp_node::binaryOp_node(exp_node *L, exp_node *R) {
+    left    = L;
+    right   = R;
+  }
+
+  
+  number_node::number_node(float value) {
+    num = value;
+   }
+  
+void number_node:: print() {
+  cout << num;
+}
+
+  float number_node::evaluate() { 
+    return num; }
+
+  variable_node::variable_node(string value) : id(value) {}
+
+void variable_node:: print() {
+  cout << id;
+}
+
+  float variable_node::evaluate() { 
+    return state[id]; 
+  }
+
+// add_node inherits the characteristics of node and adds its own evaluate function
+  // add_node's constructor just uses node's constructor
+  add_node::add_node(exp_node *L, exp_node *R) : binaryOp_node(L,R) {
+  }
+
+void add_node:: print() {
+  cout << "(";
+  left->print();
+  cout << " + ";
+  right->print();
+  cout << ")";
+}
+
+  float add_node::evaluate() {
+    float left_num, right_num;
+
+    left_num  = left->evaluate();
+    right_num = right->evaluate();
+    
+    return left_num + right_num;
+
+  }
+
+
+// subtract_node inherits the characteristics of node and adds its own evaluate function
+  subtract_node::subtract_node(exp_node *L, exp_node *R) : binaryOp_node(L,R) {
+  }
+
+void subtract_node:: print() {
+  cout << "(";
+  left->print();
+  cout << " - ";
+  right->print();
+  cout << ")";
+}
+
+  float subtract_node::evaluate() {
+    float left_num, right_num;
+
+    left_num  = left->evaluate();
+    right_num = right->evaluate();
+
+    return  left_num - right_num;
+
+  }
+
+
+// multiply_node inherits the characteristics of node and adds its own evaluate function
+  multiply_node::multiply_node(exp_node *L, exp_node *R) : binaryOp_node(L,R) {
+  }
+
+void multiply_node:: print() {
+  cout << "(";
+  left->print();
+  cout << " * ";
+  right->print();
+  cout << ")";
+}
+
+  float multiply_node::evaluate() {
+    float left_num, right_num;
+
+    left_num = left->evaluate();
+    right_num = right->evaluate();
+
+    return left_num * right_num;
+
+  }
+
+
+// divide_node inherits the characteristics of node and adds its own evaluate function
+
+  divide_node::divide_node(exp_node *L, exp_node *R) : binaryOp_node(L,R) {
+  }
+
+void divide_node:: print() {
+  cout << "(";
+  left->print();
+  cout << " / ";
+  right->print();
+  cout << ")";
+}
+
+  float divide_node::evaluate() {
+    float left_num, right_num;
+
+    left_num = left->evaluate();
+    right_num = right->evaluate();
+
+    if(right_num)
+      {
+        return  left_num / right_num;
+      }
+    else
+      {
+	cout << "division by zero -> " << left_num << " / " << 0 << endl;
+	//  include stdlib.h for exit
+	exit(1);
+      }
+  }
+
+neg_node::neg_node(exp_node *L) : exp(L) {}
+
+void neg_node:: print() {
+  cout << "- ( ";
+  exp->print();
+  cout << " )";
+}
+
+float neg_node::evaluate() {
+  float expValue = exp->evaluate();
+  return  -expValue;
+}
+
+assign_node::assign_node(string name, exp_node *expression)
+  : id(name), exp(expression) {}
+
+void assign_node::print() {
+  cout << id << " = ";
+  exp->print();
+ 
+}
+
+void assign_node::evaluate() {
+  float result = exp->evaluate();
+
+  state[id] = result;
+}
+
+print_node::print_node (exp_node *myexp) : exp(myexp) {}
+
+void print_node::print() {
+  cout <<  "print ";
+  exp->print();
+}
+
+
+void print_node::evaluate() {
+  cout << "output: " << exp->evaluate() << endl << endl;
+}
+
+skip_node::skip_node ()  {}
+
+void skip_node::evaluate() {}
+void skip_node::print() {}
+
+sequence_node::sequence_node(stmt_node* mystmt1, stmt_node* mystmt2): 
+  stmt1(mystmt1), stmt2(mystmt2)
+{}
+
+void sequence_node::print() {
+  stmt1->print();  cout << " ;" << endl;
+  stmt2->print();
+}
+
+
+void sequence_node::evaluate() {
+  stmt1->evaluate();
+  stmt2->evaluate();
+}
+
+map<string, float> state;
+
---
+++ homework_3/IMP2/imp.yacc
@@ -0,0 +1,160 @@
+%{
+
+
+/* This interpreter evaluates arithmetic expressions and assigns
+   them to the specified variable names. The grammar is:
+
+   pgm -> stmtlist
+
+   if-stmt -> if expr then stmt else stmt | if expr then stmt
+
+   stmtlist -> stmt | stmtlist ; stmt
+   stmt -> id = exp | print id | read id
+
+   logconj -> logconj || logdisj | logdisj | addsubexp
+   logdisj -> logdisj && logneg  | logneg
+   logneg  -> !compopt | compopt
+   compopt -> addsubexp == addsubexp | addsubexp >= addsubexp
+
+   exp -> exp + mulexp | exp - mulexp 
+   mulexp -> mulexp * primexp | mulexp / primexp
+   primexp ->  ( exp ) | ( exp ) | - primexp | id | number 
+*/
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <map>
+#include <list>
+#include "imp.h"
+
+
+
+using namespace std;
+
+ extern FILE *yyin;
+
+ // the root of the abstract syntax tree
+ stmt_node *root;
+
+ // for keeping track of line numbers in the program we are parsing
+ int line_num = 1;
+
+ // function prototypes, we need the yylex return prototype so C++ won't complain
+ int yylex();
+ void yyerror(const char* s);
+
+%}
+
+%start program
+
+%union {
+  float number;
+  char * var_name;
+  exp_node *exp_node_ptr;
+  stmt_node *stmt_node_ptr;
+}
+
+%error-verbose
+
+%token <number> NUMBER
+%token <var_name> ID
+%token SEMICOLON  EQUALS PRINT  PLUS MINUS TIMES DIVIDE  LPAREN RPAREN LBRACE RBRACE
+%type <exp_node_ptr> exp
+%type <exp_node_ptr> exp
+%type <exp_node_ptr> mulexp
+%type <exp_node_ptr> primexp 
+%type <stmt_node_ptr> stmtlist
+%type <stmt_node_ptr> stmt
+%type <stmt_node_ptr> program
+
+%%
+
+program : stmtlist { root = $$; }
+;
+
+stmtlist : stmtlist SEMICOLON stmt
+            { // copy up the list and add the stmt to it
+              $$ = new sequence_node($1,$3);
+            }
+         | stmtlist SEMICOLON error
+	   { // just copy up the stmtlist when an error occurs
+             $$ = $1;
+             yyclearin; } 
+         |  stmt 
+	 { $$ = $1;   }
+;
+
+stmt: ID EQUALS exp { 
+        $$ = new assign_node($1, $3);
+      }
+      | PRINT exp {
+        $$ = new print_node($2);
+      }
+      | { 
+        $$ = new skip_node();
+      }
+      | LBRACE stmtlist RBRACE { $$=$2; } 
+;
+
+logconj: logconj OR logdisj | logdisj | addsubexp
+logdisj: logdisj AND logneg  | logneg
+logneg: NOT compopt | compopt
+compopt: addsubexp BOOLEQUAL addsubexp | addsubexp BOOLGREATEREQUAL addsubexp
+
+
+exp:	exp PLUS mulexp { $$ = new add_node($1, $3); }
+
+      |	exp MINUS mulexp { $$ = new subtract_node($1, $3); }
+
+      |	mulexp {  $$ = $1; }
+;
+
+
+
+mulexp:	mulexp TIMES primexp {
+	  $$ = new multiply_node($1, $3); }
+
+      | mulexp DIVIDE primexp {
+	  $$ = new divide_node($1, $3); }
+
+      | primexp { $$=$1;  }
+;
+
+
+
+primexp:	MINUS primexp  { $$ = new neg_node($2); }
+
+      |	LPAREN exp RPAREN  {  $$ = $2; }
+
+      |	NUMBER { $$ = new number_node($1); }
+
+      | ID { $$ = new variable_node($1); }
+;
+
+
+ 
+%%
+int main(int argc, char **argv)
+{ 
+  if (argc>1) yyin=fopen(argv[1],"r");
+
+  //  yydebug = 1;
+  yyparse();
+
+  cout << "---------- list of input program------------" << endl << endl;
+
+  root -> print();
+
+  cout << "---------- exeuction of input program------------" << endl << endl;
+  
+
+  root->evaluate();
+}
+
+void yyerror(const char * s)
+{
+  fprintf(stderr, "line %d: %s\n", line_num, s);
+}
+
